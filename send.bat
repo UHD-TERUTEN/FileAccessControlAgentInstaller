e899009ed394f69574f64cf38796e1de45d1654f
droplet92 <lhs1438@gmail.com>
2021-02-03 06:03:06 +0900

    installer 구현 완료

--- send.bat
+++ send.bat
@@ -1,22 +1,22 @@
 @echo off
-@REM for /F "tokens=* USEBACKQ" %%F in (account.txt) do (
-@REM set ssh_account=%%F
-@REM )
-@REM set id_rsa_location=%HOMEDRIVE%%HOMEPATH%\.ssh\id_rsa
-@REM echo start update
-@REM echo upload log file
-@REM echo.
-@REM echo ssh connects to %ssh_account%...
-@REM ssh -i %id_rsa_location% %ssh_account% "mkdir logs"
-@REM echo.
-@REM echo sftp connects to %ssh_account%...
-@REM sftp -b upload.txt -i %id_rsa_location% %ssh_account%
-@REM echo.
-@REM echo upload log file done!
-@REM echo finish
-
-@REM echo mput > "%~dp0%filelist.txt"
-@REM forfiles /p:%systemroot%\system32\winevt\Logs /M *.evtx /D -1 >> "%~dp0%filelist.txt"
-@REM echo bye >> "%~dp0%filelist.txt"
-
-forfiles /p:%systemroot%\system32\winevt\Logs /M *.evtx /D -1 /C "CMD /C sftp put @file -i %id_rsa_location% %ssh_account%"
+for /F "tokens=* USEBACKQ" %%F in (account.txt) do (
+set ssh_account=%%F
+)
+set id_rsa_location=%HOMEDRIVE%%HOMEPATH%\.ssh\id_rsa
+echo start update
+echo upload log file
+echo.
+echo ssh connects to %ssh_account%...
+ssh -i %id_rsa_location% %ssh_account% "mkdir logs"
+ssh -i %id_rsa_location% %ssh_account% "mkdir logs/%date%"
+echo.
+echo make up a windows event log list to send 
+type nul > upload.txt
+forfiles /p:%systemroot%\System32\winevt\Logs /M *.evtx /D -1 /C "CMD /C echo put %systemroot%\system32\winevt\Logs\@file logs/%date%/" >> "%~dp0%upload.txt"
+echo bye >> "%~dp0%upload.txt"
+echo.
+echo sftp connects to %ssh_account%...
+sftp -b upload.txt -i %id_rsa_location% %ssh_account%
+echo.
+echo upload log file done!
+echo finish
