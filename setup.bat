1bad44da1df747679bfe3fb24ad0af6ce0c4eb59
droplet92 <lhs1438@naver.com>
2021-03-26 11:23:10 +0900

    설치할 때 화이트리스트 경로도 만들기

--- setup.bat
+++ setup.bat
@@ -2,6 +2,7 @@
 echo 1. Copy DetoursReject.dll to %WINDIR%\System32...
 set SOURCE_PATH=%~dp0%DetoursReject.dll
 set TARGET_PATH=%WINDIR%\System32\DetoursReject.dll
+set WHITELIST_PATH=%LOCALAPPDATA%\UHDControlServer
 copy "%SOURCE_PATH%" "%TARGET_PATH%"
 echo 2. Register FileAccessMonitor to taskschd...
 schtasks /create /tn "File Access Monitor" /sc "ONLOGON" /rl HIGHEST /tr "%~dp0%\FileAccessMonitor.exe"
@@ -9,6 +10,8 @@ echo 3. Register send.bat to taskschd...
 schtasks /create /tn "Windows Event Log Sender" /sc "DAILY" /rl HIGHEST /st 00:00 /ru system /tr "%~dp0%\send.bat"
 echo 4. Register FileAccessControlAgent to taskschd...
 schtasks /create /tn "File Access Control Agent" /sc "ONLOGON" /rl HIGHEST /tr "%~dp0%\publish\FileAccessControlAgent.exe"
+echo 5. Make a whitelist directory
+mkdir %WHITELIST_PATH%
 echo Install Succeeded. OS will automatically restarts after 10 seconds..."
 shutdown -r -t 10
 timeout 10 > NUL
