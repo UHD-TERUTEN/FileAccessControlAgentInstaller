0e124161a4bdecd9e65f3bede00320570644ca26
droplet92 <lhs1438@naver.com>
2021-04-08 02:09:47 +0900

    zip 파일로 압축해서 scp 전송

--- send.bat
+++ send.bat
@@ -3,38 +3,24 @@ for /F "tokens=* USEBACKQ" %%F in (account.txt) do (
 set ssh_account=%%F
 )
 set id_rsa_location=%HOMEDRIVE%%HOMEPATH%\.ssh\id_rsa
-set REJECT_LOGS=%LOCALAPPDATA%\FileAccessControlAgent\RejectLogs\*
 echo start update
-echo uploading reject log files
+echo zipping reject log files
+tar -cf "%DATE%.zip" -C %FileAccessControlAgentRoot%\RejectLogs .
 echo.
+echo uploading zip file
 echo ssh connects to %ssh_account%...
-ssh -i %id_rsa_location% %ssh_account% "mkdir logs"
-ssh -i %id_rsa_location% %ssh_account% "mkdir logs/RejectLogs"
+ssh -i %id_rsa_location% %ssh_account% "mkdir logs && mkdir logs/RejectLogs"
+scp -i %id_rsa_location% "%DATE%.zip" "%ssh_account%:logs/RejectLogs"
+del "%DATE%.zip"
 echo.
-del upload_reject.txt
-type nul > upload_reject.txt
-echo %REJECT_LOGS% >> "%~dp0%upload_reject.txt"
-echo bye >> "%~dp0%upload_reject.txt"
+echo zipping Windows event log files
+tar -cf "%DATE%.zip" -C %systemroot%\System32\winevt\Logs *.evtx
 echo.
-echo sftp connects to %ssh_account%...
-sftp -b upload_reject.txt -i %id_rsa_location% %ssh_account%
-echo.
-echo.
-echo uploading Windows event log files
-echo.
+echo uploading zip file
 echo ssh connects to %ssh_account%...
-ssh -i %id_rsa_location% %ssh_account% "mkdir logs/events"
-ssh -i %id_rsa_location% %ssh_account% "mkdir logs/events/%date%"
+ssh -i %id_rsa_location% %ssh_account% "mkdir logs/EventLogs"
+scp -i %id_rsa_location% "%DATE%.zip" "%ssh_account%:logs/EventLogs"
+del "%DATE%.zip"
 echo.
-echo make up a windows event log list to send 
-echo.
-del upload_evtx.txt
-type nul > upload_evtx.txt
-forfiles /p:%systemroot%\System32\winevt\Logs /M *.evtx /D -1 /C "CMD /C echo put %systemroot%\system32\winevt\Logs\@file logs/events/%date%/" >> "%~dp0%upload_evtx.txt"
-echo bye >> "%~dp0%upload_evtx.txt"
-echo.
-echo sftp connects to %ssh_account%...
-sftp -b upload_evtx.txt -i %id_rsa_location% %ssh_account%
-echo.
-echo uploading log files has done!
+echo log files have been uploaded!
 echo finish
