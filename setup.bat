8529f29233804bfe58257299779c389716c5d466
droplet92 <lhs1438@naver.com>
2021-05-13 23:16:58 +0900

    32-bit 및 64-bit 호환

--- setup.bat
+++ setup.bat
@@ -1,7 +1,10 @@
 @echo off
 echo 1. Copy DetoursReject.dll to %WINDIR%\System32...
-set SOURCE_PATH=%~dp0%DetoursReject.dll
-set TARGET_PATH=%WINDIR%\System32\DetoursReject.dll
+set SOURCE_PATH=%~dp0%DetoursReject*.dll
+set TARGET_PATH=%WINDIR%\System32\
+copy "%SOURCE_PATH%" "%TARGET_PATH%"
+set SOURCE_PATH=%~dp0%DetoursReject32.dll
+set TARGET_PATH=%WINDIR%\SysWOW64\
 copy "%SOURCE_PATH%" "%TARGET_PATH%"
 setx /M FileAccessControlAgentRoot "%LOCALAPPDATA%\FileAccessControlAgent"
 echo 2. Copy Files to %FileAccessControlAgentRoot%...
@@ -10,7 +13,8 @@ mkdir "%FileAccessControlAgentRoot%\Whitelists"
 set SOURCE=%~dp0%.
 xcopy "%SOURCE%" "%FileAccessControlAgentRoot%" /E
 echo 3. Register FileAccessMonitor to taskschd...
-schtasks /create /tn "File Access Monitor" /sc "ONLOGON" /rl HIGHEST /tr "%FileAccessControlAgentRoot%\FileAccessMonitor.exe"
+schtasks /create /tn "File Access Monitor 32-bit" /sc "ONLOGON" /rl HIGHEST /tr "%FileAccessControlAgentRoot%\FileAccessMonitor32.exe"
+schtasks /create /tn "File Access Monitor 64-bit" /sc "ONLOGON" /rl HIGHEST /tr "%FileAccessControlAgentRoot%\FileAccessMonitor64.exe"
 echo 4. Register send.bat to taskschd...
 schtasks /create /tn "Windows Event Log Sender" /sc "DAILY" /rl HIGHEST /st 12:00 /tr "%FileAccessControlAgentRoot%\send.bat"
 echo 5. Register FileAccessControlAgent to taskschd...
