f0a644d82bee16789ded7b81e680d74ccf1bfc72
droplet92 <lhs1438@naver.com>
2021-06-07 04:22:32 +0900

    업로드

--- send.bat
+++ send.bat
@@ -1,26 +1,23 @@
 @echo off
-for /F "tokens=* USEBACKQ" %%F in (%FileAccessControlAgentRoot%\account.txt) do (
+set root=%HOMEDRIVE%%HOMEPATH%
+for /F "tokens=* USEBACKQ" %%F in (%FileAccessControlAgentRoot%\scripts\account.txt) do (
 set ssh_account=%%F
 )
-set id_rsa_location=%HOMEDRIVE%%HOMEPATH%\.ssh\id_rsa
-echo start update
+set id_rsa_location=%root%\.ssh\id_rsa
+echo start uploading
 echo zipping reject log files
-mkdir "%FileAccessControlAgentRoot%\RejectLogs\%DATE%"
-move %FileAccessControlAgentRoot%\RejectLogs\*.txt "%FileAccessControlAgentRoot%\RejectLogs\%DATE%"
-tar -zcvf "%FileAccessControlAgentRoot%\RejectLogs\%DATE%\%DATE%.tar.gz" -C %FileAccessControlAgentRoot%\RejectLogs\%DATE% *.txt
-echo.
-echo zipping Windows event log files
-mkdir "%FileAccessControlAgentRoot%\EventLogs\%DATE%"
-forfiles /p:%systemroot%\System32\winevt\Logs /M *.evtx /D -1 /C "CMD /C copy %systemroot%\system32\winevt\Logs\@file %FileAccessControlAgentRoot%\EventLogs\%DATE%"
-tar -zcvf "%FileAccessControlAgentRoot%\EventLogs\%DATE%\%DATE%.tar.gz" -C %FileAccessControlAgentRoot%\EventLogs\%DATE% *.evtx
+tar -zcvf "%root%\Logs\RejectLogs\%DATE%.tar.gz" -C %root%\Logs\RejectLogs *.txt
 echo.
 echo uploading zip file
 echo ssh connects to %ssh_account%...
-ssh -i %id_rsa_location% %ssh_account% "mkdir Logs & mkdir Logs\RejectLogs & mkdir Logs\EventLogs"
-scp -i %id_rsa_location% "%FileAccessControlAgentRoot%\RejectLogs\%DATE%\%DATE%.tar.gz" "%ssh_account%:Logs/RejectLogs"
-scp -i %id_rsa_location% "%FileAccessControlAgentRoot%\EventLogs\%DATE%\%DATE%.tar.gz" "%ssh_account%:Logs/EventLogs"
-rmdir /s /q "%FileAccessControlAgentRoot%\RejectLogs\%DATE%"
-rmdir /s /q "%FileAccessControlAgentRoot%\EventLogs\%DATE%"
+ssh -i %id_rsa_location% %ssh_account% "mkdir Logs"
+scp -i %id_rsa_location% "%root%\Logs\RejectLogs\%DATE%.tar.gz" "%ssh_account%:Logs"
+del "%root%\Logs\RejectLogs\%DATE%.tar.gz"
 echo.
 echo log files have been uploaded!
 echo finish
+
+sqlite3 db.sqlite < "insert into Whitelist values(%filename%,null)"
+sqlite3 db.sqlite < update_system_logs.txt
+sqlite3 db.sqlite < update_file_access_reject_logs.txt
+sqlite3 db.sqlite < update_inquiry.txt
